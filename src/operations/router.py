1595de28ee3f4340c8b1d3935f1732436409b430
mac_pilik <diablo2435@gmail.com>
2023-05-11 18:05:44 +0300

    add router operation

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
+from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import get_async_session
 from src.operations.models import operation
+from src.operations.schemas import OperationCreate
 
 router = APIRouter(
     prefix="/operations",
@@ -12,6 +13,20 @@ router = APIRouter(
 
 @router.get("/")
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
-    query = select(operation)
-    result = await session.execute(query).where(operation.c.type == operation_type)
-    return result.all()
+    try:
+        query = select(operation)
+        result = await session.execute(query).where(operation.c.type == operation_type)
+        return result.all()
+    except:
+        return {
+            "status": "error",
+            "data": None,
+            "details": None
+        }
+
+@router.post("/")
+async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+    stmt = insert(operation).values(**new_operation.dict())
+    await session.execute(stmt)
+    await session.commit()
+    return {"status": "success"}
