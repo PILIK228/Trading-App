9b66dc9f452afff0e05ea162734dd5a110cab411
mac_pilik <diablo2435@gmail.com>
2023-05-17 12:07:43 +0300

    add pytest

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,4 +1,6 @@
+import time
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi_cache.decorator import cache
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import get_async_session
@@ -9,6 +11,13 @@ router = APIRouter(
     prefix="/operations",
     tags=["Operation"]
 )
+
+
+@router.get("/long_operation")
+@cache(expire=30)
+def get_long_op():
+    time.sleep(2)
+    return "Много много данных, которые вычислялись сто лет"
 
 
 @router.get("/")
@@ -27,6 +36,7 @@ async def get_specific_operations(operation_type: str, session: AsyncSession = D
             "data": None,
             "details": None
         })
+
 
 @router.post("/")
 async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
